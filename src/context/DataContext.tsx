0d46cc340314169acc97f2a4a4ea66f75009c753
frankmangone <francomangone18@gmail.com>
2021-07-20 08:07:58 -0300

    DataContext as DataStore, with redux-like structure

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -6,12 +6,14 @@ import { IFCWithChildren } from "../types/FCWithChildren"
 
 interface IDefaultValue {
   compounds: ICompound[]
-  setCompounds: React.Dispatch<ICompound[]>
+  addCompound: (compound: ICompound) => void
+  removeCompound: (index: number) => void
 }
 
 const defaultValue: IDefaultValue = {
   compounds: [],
-  setCompounds: () => {},
+  addCompound: () => {},
+  removeCompound: () => {},
 }
 
 // Context Provider component
@@ -22,13 +24,32 @@ export const useData = () => {
   return useContext(DataContext)
 }
 
-// Store component to abstract logic from root component
+/**
+ * Store component to abstract logic from root component
+ */
 export const DataStore: React.FC<IFCWithChildren> = (props) => {
   const { children } = props
   const [compounds, setCompounds] = useState<ICompound[]>([])
 
+  /**
+   * TODO: This logic is fairly standard... Maybe create a reusable
+   * hook to reuse it?
+   */
+  const addCompound = (compound: ICompound): void => {
+    const oldCompounds = [...compounds]
+    oldCompounds.push(compound)
+    setCompounds(oldCompounds)
+  }
+
+  const removeCompound = (index: number): void => {
+    setCompounds([
+      ...compounds.slice(0, index),
+      ...compounds.slice(index + 1, compounds.length),
+    ])
+  }
+
   return (
-    <DataContext.Provider value={{ compounds, setCompounds }}>
+    <DataContext.Provider value={{ compounds, addCompound, removeCompound }}>
       {children}
     </DataContext.Provider>
   )
