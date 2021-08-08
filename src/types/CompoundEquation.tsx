cee9b05b601902e8daa151572244a915b9810495
frankmangone <francomangone18@gmail.com>
2021-08-08 12:10:09 -0300

    Removed unnecessary types file

--- src/types/CompoundEquation.tsx
+++
@@ -1,8 +0,0 @@
-import { Token } from '../helpers/tokenization'
-
-interface IReactionEquation {
-  equationTokens: Token[]
-  coefficient: number
-}
-
-export type ICompoundEquation = IReactionEquation[]
