1fd36ba8c5c59c4ceab0ceb829add0eb4a4dc08e
Franco Mangone <francomangone18@gmail.com>
2021-10-30 22:31:01 -0300

    Compounds with different colors

--- src/hooks/useCompounds.tsx
+++ src/hooks/useCompounds.tsx
@@ -6,11 +6,18 @@ import useReactions from "./useReactions"
 import { saveToKey } from "../helpers/localStorage"
 
 /* Constants */
-import { COMPOUND_COLORS_CODES } from "../constants/compoundColors"
+import {
+  COMPOUND_COLORS,
+  COMPOUND_COLORS_CODES,
+} from "../constants/compoundColors"
 import { STORAGE_KEY } from "../features/compoundsSlice"
 
 /* Types */
 import { Compound } from "../types/Compound"
+
+interface UsedColors {
+  [key: string]: boolean
+}
 
 const useCompounds = () => {
   const dispatch = useAppDispatch()
@@ -22,13 +29,34 @@ const useCompounds = () => {
     saveToKey(compounds, STORAGE_KEY)
   }, [compounds])
 
+  const unusedColor = () => {
+    const colorKeys = COMPOUND_COLORS_CODES
+
+    // Initialize object to keep track of used colors
+    let usedColors: UsedColors = {}
+    colorKeys.forEach((key: string) => (usedColors[key] = false))
+
+    // Find currently used colors
+    compounds.forEach((compound) => (usedColors[compound.color] = true))
+
+    // Find the first unused color
+    let k = 0,
+      foundColor
+    while (k < colorKeys.length && !foundColor) {
+      if (!usedColors[colorKeys[k]]) foundColor = colorKeys[k]
+      k++
+    }
+
+    return foundColor || "blue1"
+  }
+
   return {
     compounds,
 
     addCompound: (): void => {
       const newCompound = {
         id: randomstring.generate(8),
-        color: COMPOUND_COLORS_CODES[0],
+        color: unusedColor(),
         concentration: 0,
         molecularWeight: 0,
         symbol: "A",
