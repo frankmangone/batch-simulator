66e2e8a5ee7f4429800d2415c9cc47283716cccf
Franco Mangone <francomangone18@gmail.com>
2022-03-26 14:43:13 -0300

    Integrates buttons into cards

--- src/pages/CompoundsPageNew/CompoundCard.tsx
+++ src/pages/CompoundsPageNew/CompoundCard.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components"
+import { EditIcon, DeleteIcon, RerollIcon } from "../../components/Icons"
+import { useTheme } from "../../contexts/Theme"
 
 interface CompoundCardProps {
   compound: Compound
@@ -16,16 +18,16 @@ const Wrapper = styled.div`
   box-shadow: 0 0 6px ${(props) => props.theme.getColor("baseBlack")};
   cursor: pointer;
   display: flex;
-  flex-basis: 100px;
+  flex-basis: 160px;
   flex-direction: column;
   height: 70px;
   margin: 0px 5px 10px;
   overflow: hidden;
-  padding: 0px 30px;
+  padding: 0px;
   transition: height 0.15s ease-in-out;
 
   &:hover {
-    height: 100px;
+    height: 110px;
   }
 `
 
@@ -35,23 +37,32 @@ const Symbol = styled.p<SymbolProps>`
   font-size: ${(props) => props.theme.fontSizes.h3};
   line-height: ${(props) => props.theme.lineHeights.h3};
   font-weight: 700;
-  margin: 0;
-  margin-top: 16px;
+  margin: 16px 16px 0px;
 `
 
 const Buttons = styled.div`
-  margin-top: 16px;
+  align-self: stretch;
+  display: flex;
   height: 30px;
+  justify-content: space-between;
+  margin: 16px 16px;
 `
 
 const CompoundCard: React.FC<CompoundCardProps> = (props) => {
   const { compound } = props
   const { symbol, color } = compound
 
+  const { getColor } = useTheme()
+  const white = getColor({ name: "baseBlack", shade: 500 })
+
   return (
     <Wrapper>
       <Symbol color={color}>{symbol}</Symbol>
-      <Buttons>asdasdasd</Buttons>
+      <Buttons>
+        <RerollIcon color={white} />
+        <EditIcon color={white} />
+        <DeleteIcon color={white} />
+      </Buttons>
     </Wrapper>
   )
 }
