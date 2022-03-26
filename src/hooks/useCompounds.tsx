11b0d0e4e66ed790fbd6c7a7591a834ba982df94
Franco Mangone <francomangone18@gmail.com>
2022-03-25 21:37:01 -0300

    Cleans up useCompounds hook

--- src/hooks/useCompounds.tsx
+++ src/hooks/useCompounds.tsx
@@ -44,38 +44,76 @@ const useCompounds = () => {
     return foundColor || "blue1"
   }
 
+  /**
+   * addCompound
+   *
+   * Generates a new compound in state
+   */
+  const addCompound = (): void => {
+    const newCompound = {
+      id: randomstring.generate(8),
+      color: unusedColor(),
+      concentration: 0,
+      molecularWeight: 0,
+      symbol: "A",
+      name: "",
+    }
+    dispatch(add(newCompound))
+  }
+
+  /**
+   * findCompound
+   *
+   * Finds a compound by its id and returns it - or undefined if the search yields no results
+   *
+   * @param {string} id
+   * @returns Compound | undefined
+   */
+  const findCompound = (id?: string): Compound | undefined => {
+    return compounds.find((c) => c.id === id)
+  }
+
+  /**
+   * updateCompound
+   *
+   * Updates target compound with `id`, with the new values specified in `updatedCompound`
+   *
+   * @param {string} id
+   * @param {Compound} updatedCompound
+   */
+  const updateCompound = (id: string, updatedCompound: Compound): void => {
+    dispatch(update({ id, compound: updatedCompound }))
+  }
+
+  /**
+   * removeCompound
+   *
+   * Removes compound with `id`. Also removes the compound from every reaction that contains it.
+   *
+   * @param {string} id
+   */
+  const removeCompound = (id: string): void => {
+    removeCompoundFromReactions(id)
+    dispatch(remove({ id }))
+  }
+
+  /**
+   * removeAllCompounds
+   *
+   * Removes all compounds from state. This also needs to clear all reactions.
+   */
+  const removeAllCompounds = (): void => {
+    resetReactions()
+    dispatch(reset())
+  }
+
   return {
     compounds,
-
-    addCompound: (): void => {
-      const newCompound = {
-        id: randomstring.generate(8),
-        color: unusedColor(),
-        concentration: 0,
-        molecularWeight: 0,
-        symbol: "A",
-        name: "",
-      }
-      dispatch(add(newCompound))
-    },
-
-    findCompound: (id?: string): Compound | undefined => {
-      return compounds.find((c) => c.id === id)
-    },
-
-    updateCompound: (id: string, updatedCompound: Compound): void => {
-      dispatch(update({ id, compound: updatedCompound }))
-    },
-
-    removeCompound: (id: string): void => {
-      removeCompoundFromReactions(id)
-      dispatch(remove({ id }))
-    },
-
-    removeAllCompounds: (): void => {
-      resetReactions()
-      dispatch(reset())
-    },
+    addCompound,
+    findCompound,
+    updateCompound,
+    removeCompound,
+    removeAllCompounds,
   }
 }
 
