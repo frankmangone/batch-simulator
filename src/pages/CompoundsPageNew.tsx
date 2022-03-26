db01e1376fc3eb1e7f1d93fb4714a6e0d46ba84c
Franco Mangone <francomangone18@gmail.com>
2022-03-26 13:21:09 -0300

    Sets up a new compounds page

---
+++ src/pages/CompoundsPageNew.tsx
@@ -0,0 +1,44 @@
+import styled from "styled-components"
+import Button from "../components/general/Button"
+import PageTitle from "../components/PageTitle"
+import CompoundList from "../components/compounds/CompoundList"
+import { FiPlus, FiTrash2 } from "react-icons/fi"
+import useCompounds from "../hooks/entities/useCompounds"
+import { mobileBreakpoint } from "../lib/breakpoints"
+
+const DeleteButton = styled(Button)`
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  padding: 0.6rem 1rem;
+
+  @media screen and (max-width: ${mobileBreakpoint}px) {
+    top: calc(80px + 1rem);
+  }
+`
+
+const CompoundsPage: React.FC = () => {
+  const { addCompound, removeAllCompounds } = useCompounds()
+
+  const handleResetCompounds = () => {
+    const confirmation = window.confirm(
+      "Are you sure you want to delete all compounds? This will also delete all reactions"
+    )
+    if (confirmation) removeAllCompounds()
+  }
+
+  return (
+    <>
+      <PageTitle>Compounds</PageTitle>
+      <Button color="green" onClick={addCompound}>
+        Add <FiPlus />
+      </Button>
+      <DeleteButton color="red" onClick={handleResetCompounds}>
+        <FiTrash2 size={20} />
+      </DeleteButton>
+      <CompoundList />
+    </>
+  )
+}
+
+export default CompoundsPage
