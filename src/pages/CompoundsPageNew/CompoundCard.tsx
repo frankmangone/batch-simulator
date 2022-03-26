4dabea8225569e49a175f1345aa43d7360c3e124
Franco Mangone <francomangone18@gmail.com>
2022-03-26 14:10:39 -0300

    Sets up basic card styles, hover missing

--- src/pages/CompoundsPageNew/CompoundCard.tsx
+++ src/pages/CompoundsPageNew/CompoundCard.tsx
@@ -4,15 +4,25 @@ interface CompoundCardProps {
   compound: Compound
 }
 
+interface SymbolProps {
+  color: string
+}
+
 const Wrapper = styled.div`
   background-color: ${(props) =>
     props.theme.getColor({ name: "baseBlack", shade: 800 })};
-  border-radius: 3px;
+  border-radius: 5px;
+  box-shadow: 0 0 6px ${(props) => props.theme.getColor("baseBlack")};
+  cursor: pointer;
+  display: flex;
+  flex-basis: 100px;
+  justify-content: center;
   padding: 16px 30px;
   margin: 0px 5px 10px;
 `
 
-const Symbol = styled.p`
+const Symbol = styled.p<SymbolProps>`
+  color: ${(props) => props.color};
   font-family: "Comfortaa", sans-serif;
   font-size: ${(props) => props.theme.fontSizes.h3};
   line-height: ${(props) => props.theme.lineHeights.h3};
@@ -22,10 +32,11 @@ const Symbol = styled.p`
 
 const CompoundCard: React.FC<CompoundCardProps> = (props) => {
   const { compound } = props
+  const { symbol, color } = compound
 
   return (
     <Wrapper>
-      <Symbol>{compound.symbol}</Symbol>
+      <Symbol color={color}>{symbol}</Symbol>
     </Wrapper>
   )
 }
