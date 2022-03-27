27c7dee269144e46dd58311c21672819aa31cebc
Franco Mangone <francomangone18@gmail.com>
2022-03-27 16:45:35 -0300

    Reorganizes page header

--- src/pages/CompoundsPageNew/index.tsx
+++ src/pages/CompoundsPageNew/index.tsx
@@ -9,13 +9,20 @@ const Wrapper = styled.main`
   margin: 30px auto 0px;
   max-width: 900px;
   padding: 0px 50px;
+  align-items: center;
 `
 
-const ActionButtons = styled.div`
-  margin-bottom: 20px;
+const Title = styled(PageTitle)`
+  flex: 1;
+  margin-bottom: 0;
+`
+
+const Header = styled.div`
+  margin-bottom: 40px;
   margin-top: 30px;
   display: flex;
   justify-content: flex-end;
+  align-items: center;
   align-self: stretch;
 `
 
@@ -35,15 +42,15 @@ const CompoundsPage: React.FC = () => {
 
   return (
     <Wrapper>
-      <PageTitle>Compounds</PageTitle>
-      <ActionButtons>
+      <Header>
+        <Title>Compounds</Title>
         <Button color="success" onClick={addCompound}>
           <AddIcon color="#000" size={30} />
         </Button>
         <Button color="cancel" onClick={handleResetCompounds}>
           <DeleteIcon color="#000" size={30} />
         </Button>
-      </ActionButtons>
+      </Header>
       <CompoundList />
     </Wrapper>
   )
