67b7be6047b1b1f2ee5f6f9b22b0b33b8fb7f508
Franco Mangone <francomangone18@gmail.com>
2022-04-02 11:13:16 -0300

    Adds some styles to the dropdown buttons

--- src/components/forms/SelectInputNew/SelectBody.tsx
+++ src/components/forms/SelectInputNew/SelectBody.tsx
@@ -15,14 +15,35 @@ const SelectBody = styled.div`
     ${(props) => props.theme.getColor({ name: "baseBlack", shade: 700 })};
   border-radius: 5px;
   color: ${(props) => props.theme.getColor({ name: "baseBlack", shade: 200 })};
-  cursor: pointer;
-  flex-grow: 1;
-  font-family: "Mulish", sans-serif;
-  font-size: ${(props) => props.theme.fontSizes.p};
-  font-weight: 600;
-  line-height: ${(props) => props.theme.lineHeights.p};
+  display: flex;
+  flex-direction: column;
   outline: none;
   padding: 10px;
+
+  button {
+    background-color: transparent;
+    border: none;
+    border-radius: 5px;
+    color: ${(props) =>
+      props.theme.getColor({ name: "baseBlack", shade: 200 })};
+    cursor: pointer;
+    margin-bottom: 4px;
+    padding: 6px 10px;
+    font-family: "Mulish", sans-serif;
+    font-size: ${(props) => props.theme.fontSizes.p};
+    font-weight: 600;
+    line-height: ${(props) => props.theme.lineHeights.p};
+    text-align: left;
+
+    &:hover {
+      background-color: ${(props) =>
+        props.theme.getColor({ name: "baseBlack", shade: 600 })};
+    }
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
 `
 
 export default SelectBody
