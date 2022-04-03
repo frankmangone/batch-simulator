a9a32e5fa53f51bfd037c8ca6d8bee5c72fefa82
Franco Mangone <francomangone18@gmail.com>
2022-04-03 14:31:30 -0300

    Adds some styles to the equation

--- src/pages/EditReaction/KineticEquation.tsx
+++ src/pages/EditReaction/KineticEquation.tsx
@@ -6,8 +6,15 @@ interface KineticEquationProps {
 }
 
 const EquationWrapper = styled.div`
+  align-items: center;
+  color: ${(props) => props.theme.getColor({ name: "baseBlack", shade: 200 })};
   display: flex;
   flex-basis: 100%;
+  font-family: "Comfortaa", sans-serif;
+  font-size: ${(props) => props.theme.fontSizes.h3};
+  line-height: ${(props) => props.theme.lineHeights.h3};
+  justify-content: center;
+  margin: 40px 0;
 `
 
 const KineticEquation: React.VFC<KineticEquationProps> = (props) => {
